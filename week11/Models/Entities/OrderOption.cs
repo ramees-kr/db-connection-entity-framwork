b2ef7b191a6d542182d3144fa413eb25b768dd91
ramees-kr <ramees.kr@outlook.com>
2024-04-01 13:20:41 -0400

    Added Week11 Folder

---
+++ week11/Models/Entities/OrderOption.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace week11.Models.Entities;
+
+public partial class OrderOption
+{
+    public decimal SalesTaxRate { get; set; }
+
+    public decimal FirstBookShipCharge { get; set; }
+
+    public decimal AdditionalBookShipCharge { get; set; }
+}
