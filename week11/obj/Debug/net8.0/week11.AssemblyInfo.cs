68e39be4741d456c7c7da03cfe561dd6ddfbfe45
ramees-kr <ramees.kr@outlook.com>
2024-04-01 12:52:46 -0400

    added gitignore

--- week11/obj/Debug/net8.0/week11.AssemblyInfo.cs
+++ week11/obj/Debug/net8.0/week11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("week11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+710d2914dc473379b94159ebc238f1df8a4ea47d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0c5817220c2b094a3cdcaa0006ae679f1a3bf26")]
 [assembly: System.Reflection.AssemblyProductAttribute("week11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("week11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
